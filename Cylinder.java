05b63f0c50113705bb2271dd653b5a8d3a62e23a
Joe Axberg <jaxberg@dunwoody.edu>
2021-10-10 17:28:56 -0500

    first commit

---
+++ Cylinder.java
@@ -0,0 +1,18 @@
+public class Cylinder {
+
+    public double surfaceArea(){
+
+        double surfArea = 0;
+
+        return surfArea;
+    }
+
+    public double volume(int rad, int hi){
+
+        double volume = Math.PI * rad * rad * hi;
+
+        return volume;
+
+    }
+    
+}
